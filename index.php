b19481d7354bee800ef8039b50163ceb88545b8e
Abdul Hafizh Firdaus <abdulhafizh9@gmail.com>
2021-02-19 13:58:47 +0700

    Add: get file

--- index.php
+++ index.php
@@ -4,16 +4,23 @@ $requestMethod = $_SERVER['REQUEST_METHOD'];
 
 $filename = $_GET['filename'];
 
+$response = [];
+
 if ($requestMethod === "GET") {
-    echo "GET";
+    $myfile = fopen("upload/$filename", "r") or die("Unable to open file!");
+
+    $response = [
+        "data" => json_decode(fread($myfile, filesize("upload/$filename")))
+    ];
+    fclose($myfile);
 } else if ($requestMethod === "POST") {
     $myfile = fopen("upload/$filename", "w");
     $data = file_get_contents("php://input");
     fwrite($myfile, $data);
-    
+    $response = [
+        "message" => "success"
+    ];
 }
 
 header("Content-Type: application/json");
-echo json_encode([
-    "message" => "success"
-]);
+echo json_encode($response);
