e1e984a3c426738d6197e6b104dbf06458606268
Abdul Hafizh Firdaus <abdulhafizh9@gmail.com>
2021-02-19 14:28:13 +0700

    Add: encrpytion

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 
 $requestMethod = $_SERVER['REQUEST_METHOD'];
 
-$filename = $_GET['filename'];
+$filename = isset($_GET['filename']) ? $_GET['filename'] : "";
+$secret_key = isset($_GET['key']) ? $_GET['key'] : "";
 
 $response = [];
 
@@ -14,13 +15,27 @@ if ($requestMethod === "GET") {
     ];
     fclose($myfile);
 } else if ($requestMethod === "POST") {
-    $myfile = fopen("upload/$filename", "w");
     $data = file_get_contents("php://input");
-    fwrite($myfile, $data);
+
+    $method = "aes128";
+    $iv_length = openssl_cipher_iv_length($method);
+    $iv = openssl_random_pseudo_bytes($iv_length);
+
+    $encrypted_message = openssl_encrypt($data, $method, $secret_key, 0, $iv);
+
+    
+    $myfile = fopen("upload/$filename", "w");
+    
+    fwrite($myfile, json_encode([
+        "data" => $encrypted_message
+    ]));
+
     $response = [
-        "message" => "success"
+        "message" => "success",
     ];
 }
 
 header("Content-Type: application/json");
 echo json_encode($response);
+
+$cipher = "aes-128-gcm";
