ee33a2f833a23aab6d22ddd9cc5ebefec35076bf
Abdul Hafizh Firdaus <abdulhafizh9@gmail.com>
2021-02-19 13:52:37 +0700

    Add: save file

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
 <?php
+
+$requestMethod = $_SERVER['REQUEST_METHOD'];
+
+$filename = $_GET['filename'];
+
+if ($requestMethod === "GET") {
+    echo "GET";
+} else if ($requestMethod === "POST") {
+    $myfile = fopen("upload/$filename", "w");
+    $data = file_get_contents("php://input");
+    fwrite($myfile, $data);
+    
+}
+
+header("Content-Type: application/json");
+echo json_encode([
+    "message" => "success"
+]);
