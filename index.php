19eec287cbcba07969277d435805168ebf6f482d
Abdul Hafizh Firdaus <abdulhafizh9@gmail.com>
2021-02-22 13:16:09 +0700

    Update .gitignore

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
+require __DIR__ . '/vendor/autoload.php';
+
+$dataDir = __DIR__ . "/mydb";
+
 $requestMethod = $_SERVER['REQUEST_METHOD'];
 
 $filename = isset($_GET['filename']) ? $_GET['filename'] : "";
@@ -7,54 +11,27 @@ $secret_key = isset($_GET['key']) ? $_GET['key'] : "";
 
 $response = [];
 
-$method = "aes128";
-$iv_length = openssl_cipher_iv_length($method);
-$iv = openssl_random_pseudo_bytes($iv_length);
+$configuration = [
+    "auto_cache" => true,
+    "cache_lifetime" => null,
+    "timeout" => 120,
+    "primary_key" => "_id"
+];
 
 if ($requestMethod === "GET") {
-    $myfile = fopen("upload/$filename", "r") or die("Unable to open file!");
-    $fileJSON = json_decode(fread($myfile, filesize("upload/$filename")), true);
-
-    $decrypted_message = my_decrypt($fileJSON['data'], $secret_key);
-
-    $response = [
-        "data" => json_decode($decrypted_message, true)
-    ];
-    fclose($myfile);
+    
 } else if ($requestMethod === "POST") {
-    $data = file_get_contents("php://input");
-    
-    $myfile = fopen("upload/$filename", "w");
-    
-    fwrite($myfile, json_encode([
-        "data" => my_encrypt($data, $secret_key)
-    ]));
-
-    $response = [
-        "message" => "success",
+    $newsStore = new \SleekDB\Store("news", $dataDir, $configuration);
+    $article = [
+        "title" => "Google Pixel XL",
+        "about" => "Google announced a new Pixel!",
+        "author" => [
+            "avatar" => "profile-12.jpg",
+            "name" => "Foo Bar"
+        ]
     ];
-    fclose($myfile);
+    $response[] = $newsStore->insert($article);
 }
 
 header("Content-Type: application/json");
 echo json_encode($response);
-
-
-function my_encrypt($data, $key) {
-    // Remove the base64 encoding from our key
-    $encryption_key = base64_decode($key);
-    // Generate an initialization vector
-    $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('aes-256-cbc'));
-    // Encrypt the data using AES 256 encryption in CBC mode using our encryption key and initialization vector.
-    $encrypted = openssl_encrypt($data, 'aes-256-cbc', $encryption_key, 0, $iv);
-    // The $iv is just as important as the key for decrypting, so save it with our encrypted data using a unique separator (::)
-    return base64_encode($encrypted . '::' . $iv);
-}
-
-function my_decrypt($data, $key) {
-    // Remove the base64 encoding from our key
-    $encryption_key = base64_decode($key);
-    // To decrypt, split the encrypted data from our IV - our unique separator used was "::"
-    list($encrypted_data, $iv) = explode('::', base64_decode($data), 2);
-    return openssl_decrypt($encrypted_data, 'aes-256-cbc', $encryption_key, 0, $iv);
-}
