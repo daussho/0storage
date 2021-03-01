2d5d17dea75fa3094277ad0ad0bd1a301c0a8a90
Abdul Hafizh Firdaus <abdulhafizh9@gmail.com>
2021-03-01 13:12:50 +0700

    Cleaning code

--- index.php
+++ index.php
@@ -2,21 +2,11 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-// require_once "src/Autoloader.php";
-
 use App\Helpers\GlobalHelper;
 
-$dataDir = __DIR__ . "/mydb";
-
-$requestMethod = $_SERVER['REQUEST_METHOD'];
-// if ($requestMethod !== "POST") {
-//     GlobalHelper::returnJSON([
-//         "error" => "Invalid request",
-//     ], 400);
-//     return null;
-// }
-
 $router = new AltoRouter();
+
+// Router list
 
 $router->map('GET', '/', function () {
     GlobalHelper::returnJSON([
@@ -34,7 +24,9 @@ if (is_array($match) && is_callable($match['target'])) {
     call_user_func_array($match['target'], $match['params']);
 } else {
     // no route was matched
-    header($_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+    GlobalHelper::returnJSON([
+        "error" => "Not found",
+    ], 404);
 }
 
 return null;
