ae01b6318da6ebbb964e4c3e9510627a1a84383a
Abdul Hafizh Firdaus <abdulhafizh9@gmail.com>
2021-03-31 11:45:25 +0700

    Fix: route

--- src/Settings/route.php
+++ src/Settings/route.php
@@ -11,19 +11,25 @@ $version = GlobalHelper::getAppVersion();
 
 return [
     "$version" => GlobalHelper::generateRoute("$version", [
-        ['POST', '/q/[a:document]?', function ($document) {
-            if (!in_array($document, ["admin"])) {
-                (new QueryController($document))->dbQuery();
-            }
-            throw new ResponseException(403, "You don't have access");
-        }, 'db_query'],
+        [
+            'POST',
+            '/q/[a:document]?',
+            function ($document) {
+                if (!in_array($document, ["admin"])) {
+                    (new QueryController($document))->dbQuery();
+                }
+                throw new ResponseException(403, "You don't have access");
+            },
+            'db_query'
+        ],
+
         ['GET', '/auth/login', 'App\Controllers\AuthController::login', 'auth_login'],
 
         // Admin
         ['POST', '/admin/register', 'App\Controllers\Admin\AdminController::register', 'admin_register'],
         ['POST', '/admin/login', 'App\Controllers\Admin\AdminController::login', 'admin_login'],
     ]),
-    "{$version}a" => GlobalHelper::generateRoute("{$version}a", [
+    "{$version}.a" => GlobalHelper::generateRoute("{$version}a", [
         ['POST', '/admin/login', function () {
             (new AdminController())->login();
         }, 'admin_login'],
